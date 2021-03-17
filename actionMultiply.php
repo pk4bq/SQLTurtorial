700a2154be42392988539a278ce89fdf4bddb366
pk4bq <49493273+pk4bq@users.noreply.github.com>
2021-03-17 19:56:59 -0400

    Create actionMultiply.php

---
+++ actionMultiply.php
@@ -0,0 +1,5 @@
+//Pooja Kaji
+//pk4bq
+
+Hello <?php echo htmlspecialchars($_POST['name']);?>.
+You typed the values <?php echo htmlspecialchars($_POST['value1']); ?> and <?php echo htmlspecialchars($_POST['value2']); ?>. Multipled together, the value is <?php echo htmlspecialchars($_POST['value1'])*($_POST['value2']);?>.
