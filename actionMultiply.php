5c519c25d2bfc43edbbf1149077475965e21230a
pk4bq <49493273+pk4bq@users.noreply.github.com>
2021-03-17 20:18:22 -0400

    Update and rename actionMultiply.php to library.php

--- actionMultiply.php
+++
@@ -1,5 +0,0 @@
-//Pooja Kaji
-//pk4bq
-
-Hello <?php echo htmlspecialchars($_POST['name']);?>.
-You typed the values <?php echo htmlspecialchars($_POST['value1']); ?> and <?php echo htmlspecialchars($_POST['value2']); ?>. Multipled together, the value is <?php echo htmlspecialchars($_POST['value1'])*($_POST['value2']);?>.
---
+++ library.php
@@ -0,0 +1,9 @@
+//Pooja Kaji
+//pk4bq
+
+<?php
+$SERVER = 'usersrv01.cs.virginia.edu';
+$USERNAME = 'pk4bq';
+$PASSWORD = 'XXXXX';
+$DATABASE = 'pk4bq';
+?>
